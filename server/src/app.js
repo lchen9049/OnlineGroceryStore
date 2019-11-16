49d5730020b76301e1473bf0740c802c8b57cf47
Jay Huang <jhuang71@hawk.iit.edu>
2019-11-15 18:20:03 -0600

    added some server route

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,6 @@
 const   express     = require('express'),
         path        = require('path'),
         bodyParser  = require('body-parser'),
-        cons        = require('consolidate'),    // for dust
-        dust        = require('dustjs-helpers'), // switch out to vuejs later
         cors        = require('cors'),
         morgan      = require('morgan'),
         app         = express();
@@ -10,14 +8,12 @@ const   express     = require('express'),
 
 // For Connecting to PSQL
 const {Client} = require('pg');
-const conString = "postgres://pevn_admin:pevn1234@localhost/pevndb";
+const conString = "postgres://pevn_admin:pevn1234@localhost/g_store";
 const client = new Client({
     connectionString: conString,
 });
 
-app.engine('dust', cons.dust);
 
-app.set('view engine', 'dust');
 app.set('views', __dirname + '/views');
 
 // Set Public Folder
@@ -27,16 +23,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((cors()));
-app.use(morgan('combine'));
 client.connect();
 
 
-// GET DATA FROM PSQL
+// ******************* Get all User ***************** // 
 app.get('/', function(req, res) {
   
     client.query('SELECT * FROM users', function(err, result) {
         if (err) {
-            return console.log('error running query', err);
+            return console.log('error running register query', err);
         }
         //sending data to home route
         res.send(result.rows);
@@ -44,14 +39,75 @@ app.get('/', function(req, res) {
     });
 });
 
+
+
+// ******************* REGISTER A NEW USER ***************** // 
 // insert data to psql when register route is called
 app.post('/register', (req, res) => {
-    client.query('INSERT INTO users(username, password) VALUES ($1, $2)', [req.body.email, req.body.password]);
+    client.query('INSERT INTO users(username, pword, isAdmin) VALUES ($1, $2)', [req.body.email, req.body.password]);
        
     res.redirect('/');
 });
 
+// ******************* lOGGING IN A USER ***************** // 
+app.get('/login/:uname/:pword/:isAdmin', (req, res) => {
+    const uname = req.params.uname;
+    const pword = req.params.pword;
+    const isAdmin = req.params.isAdmin;
+    console.log(isAdmin);
+    client.query('SELECT * FROM users WHERE username = $1 AND pword = $2', [uname, pword], (err, result) => {
+        if (err) {
+            return console.log('error running login query', err);
+        }
 
+        if (result.rows.length > 0) {
+            if (isAdmin == 'true') {
+                client.query('SELECT * FROM Staff where username = $1', [uname], (err, result) => {
+                    if (err) {
+                        return console.log(' error getting staff query', err);
+                    }
+
+                    res.send(result.rows);
+                })
+            } else { // customer
+                client.query('SELECT * FROM Customer where username = $1', [uname], (err, result) => {
+                    if (err) {
+                        return console.log(' error getting staff query', err);
+                    }
+
+                    res.send(result.rows);
+                })
+            }
+            
+        }
+    });
+    
+})
+
+// ******************* Add a new address to an existing user ***************** // 
+app.post('/addAddress', (req, res) => {
+    client.query('INSERT INTO Address (username, address, city, zipcode, state) VALUES ($1, $2, $3, $4, $5)', [req.body.username, req.body.address, req.body.city, req.body.zipcode, req.body.state]);
+    // do whatever we want to do, can send back data as well
+})
+
+
+// ******************* Add a new payment method to an existing user ***************** // 
+app.post('/addPayment', (req, res) => {
+    client.query('INSERT INTO Card (billing_address, card_number, card_pin) VALUES ($1, $2, $3)', [req.body.billing_address, req.body.card_number, req.body.card_pin]);
+    // do whatever
+})
+
+// *******************  Update quantity of a Product ***************** // 
+app.put('/updateQuantity', (req, res) => {
+    client.query('UPDATE Stock SET amount = $1 WHERE product_id = $2', [req.body.amount, req.body.product_id]);
+    // do whatever if needed
+})
+
+// *******************  Add Order ***************** // 
+app.post('/addOrder', (req, res) => {
+    client.query('INSERT INTO Order (username, product_id, order_status) VALUES ($1, $2, $3)', [req.body.username, req.body.product_id, req.body.order_status]);
+
+})
 
 
 app.listen(3000, function() {
