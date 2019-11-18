d402a58b16f20657e3bd9296f055ec1c7e976786
Jay Huang <jhuang71@hawk.iit.edu>
2019-11-18 14:09:59 -0600

    added seed

---
+++ seed.sql
@@ -0,0 +1,16 @@
+
+insert into product (product_name, product_price, category, p_weight, p_image)values ('banana', 3, 'fruit', 5, 'https://images.unsplash.com/photo-1566393028639-d108a42c46a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1627&q=80');
+insert into product (product_name, product_price, category, p_weight, p_image)values ('apple', 4, 'fruit', 8, 'https://images.unsplash.com/photo-1568702846914-96b305d2aaeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'); 
+insert into product (product_name, product_price, category, p_weight, p_image)values ('orange', 2, 'fruit', 6, 'https://images.unsplash.com/photo-1560749605-cebc04c2697c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'); 
+insert into product (product_name, product_price, category, p_weight, p_image)values ('grape', 4, 'fruit', 7, 'https://images.unsplash.com/photo-1515778767554-42d4b373f2b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'); 
+insert into product (product_name, product_price, category, p_weight, p_image)values ('blueberry', 6, 'fruit', 5, 'https://images.unsplash.com/photo-1498557850523-fd3d118b962e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80');
+
+insert into product (product_name, product_price, category, p_weight, p_image)values ('steak', 20, 'meat', 4, 'https://images.unsplash.com/photo-1565299715199-866c917206bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1314&q=80');
+insert into product (product_name, product_price, category, p_weight, p_image)values ('chicken', 5, 'meat', 1, 'https://images.unsplash.com/photo-1501200291289-c5a76c232e5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80');
+insert into product (product_name, product_price, category, p_weight, p_image)values ('pork', 30, 'meat', 6, 'https://images.unsplash.com/photo-1560781290-7dc94c0f8f4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80');
+
+
+insert into product (product_name, product_price, category, p_weight, p_image)values ('coke', 2, 'drink', 1, 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80');
+insert into product (product_name, product_price, category, p_weight, p_image)values ('sprite', 2, 'drink', 1, 'https://cdn.influenster.com/media/product/image/Sprite_Lemon-lime_soda_100_Natural_Flavors_7-5_oz.png.355x355_q85ss0_progressive.png');
+insert into product (product_name, product_price, category, p_weight, p_image)values ('starbuck coffee', 4, 'drink', 1, 'https://pics.drugstore.com/prodimg/416829/900.jpg');
+
